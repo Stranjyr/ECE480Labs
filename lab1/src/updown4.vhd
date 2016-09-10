1e623887b244051423537a25f21ec9814a4fdabd
William Hampton <whampton99@gmail.com>
2016-09-10 09:10:21 -0500

    Merged updown4 into src

--- lab1/src/updown4.vhd
+++ lab1/src/updown4.vhd
@@ -1,28 +1,26 @@
 library ieee;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
+use ieee.std_logic_1164.all; -- used to allow for std_logic data types
+use ieee.numeric_std.all; -- used to allow for unsigned data types
 
 entity updown4 is
   port (
 	clock 	: in std_logic;
-	dir		: in std_logic;
+	dir	: in std_logic;
 	q	: out unsigned(3 downto 0)
-
   ) ;
 end entity ; -- updown4
 
 architecture arch of updown4 is
-	signal count : unsigned(3 downto 0) := '0000';
+	signal count : unsigned(3 downto 0) := '0000'; -- internal signal that can be iteratively modified
 begin
-	q <= count;
-	process(clock)
+	q <= count; --mapping "count" to "q"
+	process(clock) --process re-evaluated on every edge of clock
 	begin
-		if rising_edge(clock) then
-			case dir is
-				when '1' => count <= count+1;
-				when others => count <= count-1;
+		if rising_edge(clock) then --on only rising edge
+			case dir is --if key is pressed
+				when '1' => count <= count+1; --count up
+				when others => count <= count-1; --if key is not pressed, count down
 			end case;
 		end if;
 	end process;
-
 end architecture ; -- arch
